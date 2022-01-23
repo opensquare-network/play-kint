9129b0eb7ccb52dbb57f63c6b0d1224a064d1acb
Yongfeng LI <wliyongfeng@gmail.com>
2022-01-23 14:25:55 +0800

    Add script to create and query escrow lock

---
+++ src/escrow/createLock.js
@@ -0,0 +1,23 @@
+const { getApi, disconnect } = require("../api");
+const { createTestKeyring } = require("@polkadot/keyring");
+
+(async function createLock() {
+  const api = await getApi();
+
+  const instance = createTestKeyring()
+  const charlie = instance.pairs[4];
+  const unsub = await api.tx.escrow.createLock(
+    0.1 * Math.pow(10, 12),
+    100000
+  ).signAndSend(charlie, (result) => {
+    console.log(`Current status is ${ result.status }`);
+
+    if (result.status.isInBlock) {
+      console.log(`Transaction included at blockHash ${ result.status.asInBlock }`);
+    } else if (result.status.isFinalized) {
+      console.log(`Transaction finalized at blockHash ${ result.status.asFinalized }`);
+      unsub();
+      disconnect();
+    }
+  });
+})()
---
+++ src/escrow/locks.js
@@ -0,0 +1,30 @@
+const { getApi, disconnect } = require("../api");
+const { encodeAddress } = require("@polkadot/util-crypto");
+const { allNetworks } = require("@polkadot/networks");
+
+const kintsugi = allNetworks.find(network => network.network === 'kintsugi');
+
+function normalizeEntry(api, [key, value]) {
+  const pubKeyU8a = key.slice(48, 80);
+  const addr = encodeAddress(pubKeyU8a, kintsugi.prefix);
+
+  return {
+    addr,
+    locked: {
+      amount: value.amount.toString(),
+      end: value.end.toNumber(),
+    }
+  }
+}
+
+async function getAllLocks() {
+  const api = await getApi()
+
+  const entries = await api.query.escrow.locked.entries()
+  const allLocks = entries.map(entry => normalizeEntry(api, entry));
+  console.log(allLocks)
+
+  await disconnect()
+}
+
+getAllLocks();
