2b7fe78683b0a71a003a3d2415356273d05302e2
Yongfeng LI <wliyongfeng@gmail.com>
2022-07-21 22:25:36 +0800

    Update

--- src/tokens/balance.js
+++ src/tokens/balance.js
@@ -5,10 +5,12 @@ const { getTreasuryAccount } = require("../account/treasuryAccount");
 ;(async function () {
   const api = await getApi();
 
+  const address = 'a3c5XSG3msKgh1T1Cu8ig6CDrHA3qyxJVMWkMENGCjvZGCsWb';
+
   const burn = api.consts.treasury.burn
   const treasuryAccount = getTreasuryAccount(api);
-  const accountData = await api.query.tokens.accounts(treasuryAccount, { token: monetary.Kintsugi.ticker });
-  console.log(accountData);
+  const accountData = await api.query.tokens.accounts(address, { token: monetary.Kintsugi.ticker });
+  console.log(accountData.toJSON());
 
   await disconnect();
 })()
